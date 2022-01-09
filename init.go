51078cfa74e6656724e0926c5761eb947481d30f
Administrator <1150383838@qq.com>
2022-01-10 07:24:46 +0800

    测试

--- init.go
+++ init.go
@@ -4,11 +4,18 @@ package appStoreApi
 type Api struct {
 	token string
 }
+
 // NewApiToken 使用token
-func NewApiToken(token string) error  {
+func NewApiToken(token string) error {
 	return nil
 }
+
 // NewApiSecret 使用P8
 func NewApiSecret() error {
+	return nil
+}
+
+// Test 使用P8
+func Test() error {
 	return nil
 }
